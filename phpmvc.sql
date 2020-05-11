e8f681820209798f0e4926533c8119dd0c05e79f
HeldiTioPratama <pratamahelditio@gmail.com>
2020-05-11 20:15:27 +0700

    sql

---
+++ phpmvc.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 11, 2020 at 03:14 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpmvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mhs`
+--
+
+CREATE TABLE `mhs` (
+  `id` int(255) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `umur` int(5) NOT NULL,
+  `jurusan` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `mhs`
+--
+
+INSERT INTO `mhs` (`id`, `nama`, `umur`, `jurusan`) VALUES
+(1, 'Heldi Tio ', 10, 'Teknik Informatika'),
+(17, 'Raka', 20, 'Teknik Elektro'),
+(18, 'Adibul', 20, 'Teknik Mekatronika'),
+(19, 'Afsal', 20, 'Teknik Mekatronika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `mhs`
+--
+ALTER TABLE `mhs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `mhs`
+--
+ALTER TABLE `mhs`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
